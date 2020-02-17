0a1b5fd848bb272d8faa0f0c92e9c92be982ae87
Vidhya Arjun <vidhyasarjun@gmail.com>
2020-02-17 14:50:44 +0100

    adding changes

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+class Arithmetic 
+{
+    public static void main(String arg[])
+    {
+        int a = 10;
+        int b = 5;
+        System.out.println("Sum value " +(a+b));
+        System.out.println("Difference is " +(a-b));
+        System.out.println("Product " +(a*b));
+        System.out.println("Divsion "+(a/b));
+        System.out.println("Modulus "+(a%b));
+        System.out.println("Increment " +(++a));
+        System.out.println("Decrement " +(--b));
+
+    }
+
