1d829b01de883897934cf90c179adc214c5adb29
Vidhya Arjun <vidhyasarjun@gmail.com>
2020-02-17 14:37:14 +0100

    adding Arithmetic example to git

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+class Arithmetic
+{
+    public static int add(int a)
+    {
+      // System.out.println("Sum" +(a+b));
+       return a ;	 
+      
+    }
+    public static void main(String arg[])
+    {
+       int sum = add(10);
+    }
+}
