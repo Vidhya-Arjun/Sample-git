b5ebd21a901d238d22566f565c58957c3c9fb00d
Vidhya Arjun <vidhyasarjun@gmail.com>
2020-02-17 13:09:30 +0100

    adding Basic.java

---
+++ Basic.java
@@ -0,0 +1,17 @@
+public class Basic
+{
+    public static void main(String args[])
+    {
+        String name = "Vidhya";
+        String dob = "23-11-1991";
+        int age = 28;
+        String address = "Stopvagen 51, Brommaplan, Bromma 16835";
+        String phonenumber = "234567890" ;
+        System.out.println(name);
+        System.out.println(dob);
+        System.out.println(age);
+        System.out.println(address);
+        System.out.println(phonenumber);
+
+    }
+} 
