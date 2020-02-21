dd1d40290ce517fb1ad32ac67755a40cce8b2fa9
Vidhya Arjun <vidhyasarjun@gmail.com>
2020-02-21 15:17:23 +0100

    Sum of natural number

---
+++ Sumofnumber.java
@@ -0,0 +1,15 @@
+public class Sumofnumber
+{
+public static void main(String arg[])
+ {
+	int num = 10, count = 1, total = 0;
+	
+	while(count <= num)
+	{
+	  total = total + count;
+	  count++;
+	}   
+	System.out.println("Sum of first ten naturalis: " +total);
+ 
+ }
+}
